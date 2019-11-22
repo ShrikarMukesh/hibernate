5f36d0d20cf1d4cb7f5d76a98fa6808e0b11e74f
Shrikar <Shrikar@DESKTOP-RJLKK58>
2019-11-23 01:57:30 +0530

    Derek banas code

---
+++ hibernate-derekbanas/src/main/java/com/elevens/hibernate_derekbanas/App.java
@@ -0,0 +1,13 @@
+package com.elevens.hibernate_derekbanas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
